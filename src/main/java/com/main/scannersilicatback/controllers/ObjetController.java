34d53606321f0f59d5692d463dd577b366bc00e3
maximelonguepe <longuepemaxime73@gmail.com>
2023-09-17 19:40:00 +0200

    Adding delete mapping

--- src/main/java/com/main/scannersilicatback/controllers/ObjetController.java
+++ src/main/java/com/main/scannersilicatback/controllers/ObjetController.java
@@ -1,13 +1,16 @@
 package com.main.scannersilicatback.controllers;
 
 import com.main.scannersilicatback.entities.Objet;
+import com.main.scannersilicatback.repositories.ConsommableRepository;
 import com.main.scannersilicatback.repositories.ObjetRepository;
+import com.main.scannersilicatback.repositories.ProfilRepository;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +21,44 @@ import java.util.Optional;
 public class ObjetController {
     @Autowired
     private ObjetRepository objetRepository;
+
+
+    @Autowired
+    private ConsommableRepository consommableRepository;
+
     @GetMapping
-    public List<Objet> findAll(){
+    public List<Objet> findAll() {
+        return objetRepository.findAll();
+    }
+
+    @GetMapping("filter")
+    public List<Objet> filterObjects(@PathParam("type") String type, @PathParam("id") String id) {
+        Integer idInt = 0;
+        if (id != null) {
+            idInt = Integer.parseInt(id);
+
+        }
+        if (type.equals("PROFIL")) {
+            if (idInt == 0) {
+                return objetRepository.findByType("PROFIL");
+            } else {
+                return objetRepository.findByTypeAndId("PROFIL", idInt);
+            }
+        }
+        else if (type.equals("CONSOMMABLE")) {
+            if (idInt == 0) {
+                return objetRepository.findByType("CONSOMMABLE");
+            } else {
+                return objetRepository.findByTypeAndId("CONSOMMABLE", idInt);
+            }
+        }
+        else if(idInt!=0){
+            List<Objet> objets = new ArrayList<>();
+            Optional<Objet> objetOptional = objetRepository.findById(idInt);
+            objetOptional.ifPresent(objets::add);
+            return objets;
+
+        }
         return objetRepository.findAll();
     }
 
@@ -28,5 +67,15 @@ public class ObjetController {
         Optional<Objet> objetOptional = objetRepository.findById(id);
         return objetOptional.map(objet -> new ResponseEntity<>(objet, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
 
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Void> deleteObjectById(@PathParam("id") Integer id){
+        try{
+            objetRepository.deleteById(id);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/main/scannersilicatback/repositories/ObjetRepository.java
+++ src/main/java/com/main/scannersilicatback/repositories/ObjetRepository.java
@@ -3,5 +3,12 @@ package com.main.scannersilicatback.repositories;
 import com.main.scannersilicatback.entities.Objet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ObjetRepository extends JpaRepository<Objet,Integer> {
+
+    List<Objet> findByType(String type);
+
+    List<Objet> findByTypeAndId(String type,Integer id);
 }
